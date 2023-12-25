81b6fee2c24c4031ca748be602f72eea67af439d
woowonlee <lww1224@gmail.com>
2023-12-25 21:07:18 +0900

    2023-12-25

--- ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -32,4 +32,9 @@ export class RecipeDetailComponent implements OnInit {
       }
     )
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
 }
--- ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
     constructor(private slService: ShoppingListService) {}
 
+    recipesChanged = new Subject<Recipe[]>
 
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://picsum.photos/id/237/200/300', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)])
@@ -23,5 +25,20 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.getRecipes());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.getRecipes());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.getRecipes());
     }
 }
