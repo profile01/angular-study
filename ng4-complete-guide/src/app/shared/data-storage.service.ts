4f05865ad4b4a3bd564e86c569ce4b5631182943
woowonlee <lww1224@gmail.com>
2024-01-21 17:16:54 +0900

    2024-01-21

--- ng4-complete-guide/src/app/shared/data-storage.service.ts
+++ ng4-complete-guide/src/app/shared/data-storage.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs';
+import { exhaustMap, map, pipe, take, tap } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -34,8 +39,8 @@ export class DataStorageService {
             };
           });
         }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
         })
       );
   }
